a1e26dff868b85d55dd62f4ac3ebcba2c83c9c89
Nick Aguilos <nick.aguilos@gmail.com>
2025-12-10 02:23:35 +0800

    adding bootstrap js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,5 @@ document.getElementById('menuBtn')?.addEventListener('click',()=>{
   m.classList.toggle('hidden');
 });
 document.getElementById('year').textContent = new Date().getFullYear();
+
+// import '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
